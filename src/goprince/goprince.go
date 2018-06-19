439e9edba29d5b9393f7ac6f643f52b272a2183e
Spoon4 <spoon4@gmail.com>
2018-06-19 20:49:53 +0200

    first environment test

--- src/goprince/goprince.go
+++ src/goprince/goprince.go
@@ -12,9 +12,19 @@ func indexHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprintf(w, "This is the RESTful api")
 }
 
+func luckyHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	name := r.URL.Query()["name"][0]
+	fmt.Fprintf(w, "Hello, %q\nYour lucky number  is: %d", name, generateLuckyNumber(name))
+}
+
+func generateLuckyNumber(name string) int {
+	return len(name)
+}
+
 func main() {
 	router := httprouter.New()
 	router.GET("/", indexHandler)
+	router.GET("/lucky", luckyHandler)
 
 	// print env
 	env := os.Getenv("APP_ENV")
@@ -26,4 +36,3 @@ func main() {
 
 	http.ListenAndServe(":8080", router)
 }
-
